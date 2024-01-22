a038cf3bc2b8b25820e7a5bcfa65b3e679fb84b9
samszo <samszon@gmail.com>
2024-01-22 10:00:05 +0100

    import to omk with pdf export

---
+++ modules/authParams.js
@@ -0,0 +1,7 @@
+export const pa = {
+    'navbar':d3.select('#navbarMain'),
+    mail:'samuel.szoniecky@univ-paris8.fr',
+    apiOmk:'http://localhost/omk_imagestrompeuses/api',
+    ident: 'mKqCaH0WFxM1F8VSD9e6KviiGT8bAntF',
+    key: 'iLwn2CXj3NWJ0NQhHWBc0F9qZmU9M3dO',
+}
